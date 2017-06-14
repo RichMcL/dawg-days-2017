b6fdf21b51ba04726f669aa76e2d0cf6d1fb42cd
Rich McLaughlin <rsmclaug@gmail.com>
2017-06-14 15:00:30 -0400

    login code formatting

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,55 +1,44 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { AlertController, NavController, NavParams } from 'ionic-angular';
+
 import { TabsPage } from '../tabs/tabs';
 import { AuthProvider } from '../../providers/auth/auth';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 export class LoginPage {
-  email: string = "";
-  password: string = "";
+    email: string = "";
+    password: string = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public authProvider: AuthProvider, private alertCtrl: AlertController, public storage: Storage) {
-  }
+    constructor(private navCtrl: NavController, private navParams: NavParams, private authProvider: AuthProvider, private alertCtrl: AlertController, private storage: Storage) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+    signin() {
+        this.authProvider.signin({ email: this.email, password: this.password }).then((data) => {
+            this.storage.set('uid', data.uid);
+            this.navigateToMainPage();
+        }).catch((error) => {
+            let alert = this.alertCtrl.create({
+                title: 'Nope!',
+                message: error.message ? error.message : error.toString(),
+                buttons: ['Dismiss']
+            });
+            alert.present();
+        });
+    }
 
-  signin() {
-    this.authProvider.signin({ email: this.email, password: this.password }).then((data) => {
-      this.storage.set('uid', data.uid);
-      this.navigateToMainPage();
-    }).catch((error) => {
-      let alert = this.alertCtrl.create({
-        title: 'Nope!',
-        message: error.message ? error.message : error.toString(),
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    });
-  }
+    createAccount() {
+        this.authProvider.createAccount(this.email, this.password).then(user => {
+            this.navigateToMainPage();
+        }, error => {
+            console.log('error creating account! error: ' + error);
+        });
+    }
 
-  createAccount() {
-    this.authProvider.createAccount(this.email, this.password).then(user => {
-      console.log('successfully created account! user: ' + user);
-      this.navigateToMainPage();
-    }, error => {
-      console.log('error creating account! error: ' + error);
-    });
-  }
-
-  navigateToMainPage() {
-    this.navCtrl.push(TabsPage);
-  }
-
+    navigateToMainPage() {
+        this.navCtrl.push(TabsPage);
+    }
 }
