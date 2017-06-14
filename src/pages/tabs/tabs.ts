a6d4ca28cf5ba7bb15ff5c822056a51b159a2194
Rich McLaughlin <rsmclaug@gmail.com>
2017-06-14 15:03:36 -0400

    tabs files formatting

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import {UsersPage} from "../users/users";
-import {AccountPage} from "../account/account";
+import { UsersPage } from "../users/users";
+import { AccountPage } from "../account/account";
 
-/**
- * Generated class for the TabsPage tabs.
- *
- * See https://angular.io/docs/ts/latest/guide/dependency-injection.html for
- * more info on providers and Angular DI.
- */
 @Component({
-  selector: 'page-tabs',
-  templateUrl: 'tabs.html'
+    selector: 'page-tabs',
+    templateUrl: 'tabs.html'
 })
 export class TabsPage {
+    usersRoot = UsersPage;
+    accountRoot = AccountPage;
 
-  usersRoot = UsersPage;
-  accountRoot = AccountPage;
-
-
-  constructor(public navCtrl: NavController) {}
+    constructor(private navController: NavController) { }
 
 }
